335620a066ce92a602c7d020b46daf869a77dfb0
Sylvain <sylvain.debrigode@pernod-ricard.com>
2025-12-11 08:40:26 +0100

    Add files via upload

---
+++ i18n/en.js
@@ -0,0 +1,95 @@
+var en = {
+	menu: {
+		portfolio: "Portfolio",
+		about: "About me",
+	},
+
+	show_more: {
+		more: "See more +",
+		less: "See less -",
+	},
+	
+	choixportfolio: {
+		tout: "All",
+	},
+
+	presentation: {
+		description: "Welcome to my portfolio, I'm a 3D artist. Here, you can find out who I am and which projects I have been working on.",
+		cv: "Download my resume",
+	},
+
+	freelance: {
+		titre: "Freelance - 2020-Today",
+		description: "3D/2D Artist",
+	},
+
+	horus: {
+		description: "Computer technician",
+		more: "Fixing issues and installation of computer systems in store (POS, cash registers, printers, etc.). (Retail)",
+	},
+
+	pacifa: {
+		description: "3D graphic designer",
+		more: "3D modeling of entertainment structures for aerial and then 360 ° visualization in online ticketing. <br> Integration of architectural plans in .DWG or .JPG for modeling in 3DS Max. Textures done in Adobe Photoshop, lighting then rendered in V-Ray. <br> Process automation with MAXScript.",
+	},
+
+	lycee: {
+		titre: "High school Paul Lapie - Lunéville - 2012",
+		description: "High school supervisor",
+	},
+
+	imprimerie: {
+		titre: "Municipal printing - Lunéville - 2011",
+		description: "Intership",
+		more: "Design conception in Adobe InDesign until "+"Ready for press"+" then printing, trimming and delivery to the client.",
+	},
+
+	cv: {
+		formation: "EDUCATION",
+	},
+	
+	formationsubstance: {
+		titre:
+			"Substance Designer & Painter training - 2020",
+	},
+
+	licence: {
+		titre:
+			"Licence professionnelle Image et Son Numériques (Professional license in Image and Digital Sounds) - 2012-2013",
+	},
+
+	dut: {
+		titre:
+			"Graduation in DUT Services et Réseaux de Communication (equivalent to Higher National Diploma in Services and Communication Networks) - 2011",
+	},
+
+	bac: {
+		titre:
+			"Graduation in Baccalauréat Scientifique (High School diploma, scientific option) - 2009",
+	},
+
+	skills : {
+		titre : "SKILLS",
+		softwares : "Softwares",
+		langues : "Languages",
+		francais : "French",
+		anglais : "English",
+		secondary : "Secondary skills",
+	},
+
+	loisirs: {
+		titre: "HOBBIES",
+		karate: "Karate - 12 years",
+		athletisme: "Athletics - 2 years",
+		rugby: "Rugby - 1 year",
+		volley: "Volley-ball - 1 year",
+		batterie: "Drums - 2 years",
+		dessin: "Drawing, Painting, Fimo clay",
+		couture: "Sewing",
+		spa: "Volunteer in animal protection",
+	},
+	
+	softsused : {
+		titre : "Softwares",
+	},
+};
---
+++ i18n/fr.js
@@ -0,0 +1,95 @@
+var fr = {
+	menu: {
+		portfolio: "Portfolio",
+		about: "À propos",
+	},
+	
+	show_more: {
+		more: "Voir plus +",
+		less: "Voir moins -",
+	},
+
+	choixportfolio: {
+		tout: "Tout",
+	},
+
+	presentation: {
+		description: "Bienvenue sur mon portfolio, je suis artiste 3D. Je vous propose ici de découvrir qui je suis ainsi que les projets sur lesquels j’ai pu travailler.",
+		cv: "Télécharger mon CV",
+	},
+
+	freelance: {
+		titre: "Freelance - 2020-Aujourd'hui",
+		description: "Infographiste 3D/2D",
+	},
+	
+	horus: {
+		description: "Technicienne en informatique",
+		more: "Dépannage et installation de systèmes informatiques en magasin (TPE, caisses, imprimantes etc). (Retail)",
+	},
+
+	pacifa: {
+		description: "Infographiste 3D",
+		more: "Modélisation 3D de structures de divertissement pour une visualisation aérienne puis à 360° dans la billetterie en ligne. <br> Intégration de plans d'architectes en .DWG ou .JPG pour modélisation sous 3DS Max. Textures sous Adobe Photoshop, éclairage puis rendu sous V-Ray. <br> Automatisation de procédés avec MAXScript.",
+	},
+
+	lycee: {
+		titre: "Lycée Paul Lapie - Lunéville - 2012",
+		description: "Assistante d'éducation",
+	},
+
+	imprimerie: {
+		titre: "Imprimerie Municipale - Lunéville - 2011",
+		description: "Stage",
+		more:"Conception des documents sous Adobe InDesign jusqu'au BAT puis impression, massicotage et livraison au client.",
+	},
+
+	cv: {
+		formation: "FORMATION",
+	},
+	
+	formationsubstance: {
+		titre:
+			"Formation Substance Designer & Painter - 2020",
+	},
+
+	licence: {
+		titre:
+			"Licence professionnelle Image et Son Numériques (ISN) - 2012-2013",
+	},
+
+	dut: {
+		titre:
+			"Obtention du DUT en Services et Réseaux de Communication (SRC) - 2011",
+	},
+
+	bac: {
+		titre: "Obtention du Bac Scientifique spécialité Mathématiques - 2009",
+	},
+
+	skills : {
+		titre : "COMPÉTENCES",
+		softwares : "Logiciels",
+		langues : "Langues",
+		francais : "Français",
+		anglais : "Anglais",
+		secondary : "Compétences secondaires",
+	},
+		
+	loisirs : {
+		titre : "LOISIRS",
+		karate : "Karaté - 12 ans",
+		salle : "Sport en Salle - 2 ans",
+		athletisme : "Athlétisme - 2 ans",
+		rugby : "Rugby - 1 an",
+		volley : "Volley-ball - 1 an",
+		batterie : "Batterie - 2 ans",
+		dessin : "Dessin, Peinture, Pâte Fimo",
+		couture : "Couture",
+		spa : "Bénévole en SPA",
+	},
+	
+	softsused : {
+		titre : "Logiciels utilisés",
+	},
+};
